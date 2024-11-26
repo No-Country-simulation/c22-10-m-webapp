9bd563b3408493c7b4ba300c3abe3232715fb34c
BenjaMoreno <webenja0@gmail.com>
2024-11-26 19:34:42 -0300

    Update barra de navegación

--- Frontend/src/COMPONENTS/Layout/Cabecera.jsx
+++ Frontend/src/COMPONENTS/Layout/Cabecera.jsx
@@ -1,107 +1,126 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Navbar, Nav, Container, Col, Row, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
 import Buscador from "../Common/Buscador";
+import {
+  HeartIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
+import "./Cabecera.css";
 
-function Cabecera() {
+const Cabecera = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Container>
-            <Row>
-              <Col xs={4}>
-                <Navbar.Brand as={Link} to="/">
-                  Pacis
-                </Navbar.Brand>
-              </Col>
-              <Col xs={4}>
-                <Buscador />
-              </Col>
-              <Col className="text-end">
-                <Nav.Link as={Link} to="/auth">
-                  Iniciar sesión
-                </Nav.Link>
-              </Col>
-            </Row>
-            <hr />
-            <Row>
-              <Col className="text-center">
-                <NavDropdown title="Productos" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">
-                    Bombas de sal
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Jabones artesanales
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Sales de baño
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">
-                    Velas aromáticas
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">
-                    Aceites
-                  </NavDropdown.Item>{" "}
-                  <NavDropdown.Item href="#action/3.4">
-                    Exfoliantes
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">
-                    Mascarillas
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">Cremas</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">
-                    Esponjas y pads
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">
-                    Bálsamos labiales
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.4">Guayas</NavDropdown.Item>
-                </NavDropdown>
-              </Col>
-              <Col className="text-center">
-                <NavDropdown title="Kits de baño" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">
-                    <strong>Kit 1</strong>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Día</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Noche</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.1">
-                    <strong>Kit 2</strong>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Día</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Noche</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.1">
-                    <strong>Kit 3</strong>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Día</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Noche</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.1">
-                    <strong>Kit 4</strong>
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Día</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Noche</NavDropdown.Item>
-                </NavDropdown>
-              </Col>
-              <Col className="text-center">
-                <Nav.Link as={Link} to="/populares">
-                  Productos populares
-                </Nav.Link>
-              </Col>
-              <Col className="text-center" md="auto">
-                Carrito
-              </Col>
-            </Row>
-          </Container>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header>
+      {/* Primer Navbar */}
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Row className="w-100">
+            <Col xs={6} sm={4} md={3}>
+              <Navbar.Brand as={Link} to="/">
+                <Image src="" rounded />
+                Mi Logo
+              </Navbar.Brand>
+            </Col>
+            <Col xs={12} sm={8} md={5}>
+              <Buscador />
+            </Col>
+            <Col xs={12} sm={4} md={4}></Col>
+          </Row>
+        </Container>
+      </Navbar>
+
+      {/* Segundo Navbar con Dropdowns de Productos y Kits */}
+      <Navbar bg="secondary" expand="md" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="navbar-main" />
+          <Navbar.Collapse id="navbar-main">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/populares">
+                Productos Populares
+              </Nav.Link>
+
+              {/* Dropdown para Productos */}
+              <NavDropdown
+                title="Productos"
+                id="productos-dropdown"
+                className="hover-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">
+                  Bombas de sal
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Jabones artesanales
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Sales de baño
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Velas aromáticas
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Aceites</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Exfoliantes
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Mascarillas
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Cremas</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Esponjas y pads
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Balsamos labiales
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Guayas</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Reseñas</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Crear reseñas
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Armá tu kit
+                </NavDropdown.Item>
+              </NavDropdown>
+
+              <Nav.Link>Kits</Nav.Link>
+            </Nav>
+
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/auth">
+                <UserIcon
+                  style={{
+                    color: "rgba(240, 248, 255, 0.616)",
+                    height: "24px",
+                    width: "24px",
+                  }}
+                />
+                Ingresar
+              </Nav.Link>
+              <Nav.Link href="#soporte">
+                <HeartIcon
+                  style={{
+                    color: "rgba(240, 248, 255, 0.616)",
+                    height: "24px",
+                    width: "24px",
+                  }}
+                />
+                Favoritos
+              </Nav.Link>
+              <Nav.Link href="#soporte">
+                <ShoppingBagIcon
+                  style={{
+                    color: "rgba(240, 248, 255, 0.616)",
+                    height: "24px",
+                    width: "24px",
+                  }}
+                />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
-}
+};
 
 export default Cabecera;
