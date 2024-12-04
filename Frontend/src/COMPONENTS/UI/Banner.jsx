4b7ee95f7d5df09c8c2dfd5e7b310ce6c2fc8e80
BenjaMoreno <webenja0@gmail.com>
2024-12-04 13:35:28 -0300

    avances banner

--- Frontend/src/COMPONENTS/UI/Banner.jsx
+++ Frontend/src/COMPONENTS/UI/Banner.jsx
@@ -1,6 +1,8 @@
 import Slider from "react-slick";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 import foto from "../../IMAGES/WEBP/fotocl.webp";
 import "./Banner.css";
+
 export const Banner = () => {
   const settings = {
     dots: true,
@@ -10,21 +12,106 @@ export const Banner = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     waitForAnimate: false,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    appendDots: (dots) => (
+      <div
+        style={{
+          position: "absolute",
+          bottom: "50px", // Ajusta la altura desde abajo
+          zIndex: 1,
+        }}
+      >
+        <ul style={{ margin: "0" }}>{dots}</ul>
+      </div>
+    ),
   };
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={`${className} custom-arrow`} // Mantén la clase predeterminada
+        style={{
+          ...style,
+          display: "block",
+          position: "absolute",
+          right: "5vw",
+          zIndex: 1,
+          height: "40px",
+          width: "40px",
+          borderRadius: "50%",
+          overflow: "hidden",
+        }}
+        onClick={onClick}
+      >
+        {/* Flecha predeterminada */}
+        <ArrowRightIcon
+          style={{
+            color: "#ffffff",
+            position: "absolute",
+            left: "0",
+            backgroundColor: "rgb(66, 55, 53)",
+            padding: "8px",
+          }}
+        />
+      </div>
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={`${className} custom-arrow`} // Mantén la clase predeterminada
+        style={{
+          ...style,
+          display: "block",
+          position: "absolute",
+          left: "5vw",
+          zIndex: 1,
+          height: "40px",
+          width: "40px",
+          borderRadius: "50%",
+          overflow: "hidden",
+        }}
+        onClick={onClick}
+      >
+        {/* Flecha predeterminada */}
+        <ArrowLeftIcon
+          style={{
+            color: "#ffffff",
+            position: "absolute",
+            left: "0",
+            backgroundColor: "rgb(66, 55, 53)",
+            padding: "8px",
+          }}
+        />
+      </div>
+    );
+  }
+
   return (
     <Slider {...settings} className="slider-banner">
-      <div className="item-banner">
-        <img src={foto} />
-      </div>
-      <div className="item-banner">
-        <img src={foto} />
-      </div>
-      <div className="item-banner">
-        <img src={foto} />
-      </div>
-      <div className="item-banner">
-        <img src={foto} />
-      </div>
+      <figure className="item-banner">
+        <img src={foto} alt="Banner" />
+        <figcaption>Texto descriptivo</figcaption>
+      </figure>
+      <figure className="item-banner">
+        <img src={foto} alt="Banner" />
+        <figcaption>Texto descriptivo</figcaption>
+      </figure>
+      <figure className="item-banner">
+        <img src={foto} alt="Banner" />
+        <figcaption>Texto descriptivo</figcaption>
+      </figure>
+      <figure className="item-banner">
+        <img src={foto} alt="Banner" />
+      </figure>
+      <figure className="item-banner">
+        <img src={foto} alt="Banner" />
+        <figcaption>Texto descriptivo</figcaption>
+      </figure>
     </Slider>
   );
 };
--- Frontend/src/PAGES/Home.jsx
+++ Frontend/src/PAGES/Home.jsx
@@ -6,15 +6,17 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <section className="home" style={{ overflow: "hidden" }}>
-      <div style={{ height: "70vh", width: "100vw", margin: "8vh auto" }}>
+      <div
+        style={{
+          height: "82vh",
+          width: "100vw",
+        }}
+      >
         <Container
-          className="w-100 h-100 "
-          style={{ position: "relative", border: " solid black 1px" }}
+          className="w-100 h-100 m-0 p-0 "
+          style={{ position: "relative" }}
         >
           <Banner />
-          <Link className="link-vermas" to="/">
-            Ver más
-          </Link>
         </Container>
       </div>
       <div style={{ height: "auto", width: "100vw" }}>
